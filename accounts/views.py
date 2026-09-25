e8db8874ba4261911122998dac3070ce29dc48c3
maliktiayon <301201648+workcmtiayon-arch@users.noreply.github.com>
2026-09-25 05:36:26 +0100

    feat: add signup and login views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views import View
+
+from .forms import SignUpForm
+
+
+class SignUpView(View):
+    template_name = 'accounts/signup.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('chat_page')
+        return render(request, self.template_name, {'form': SignUpForm()})
+
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('chat_page')
+        return render(request, self.template_name, {'form': form})
+
+
+class AccountLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    redirect_authenticated_user = True
+    next_page = reverse_lazy('chat_page')
